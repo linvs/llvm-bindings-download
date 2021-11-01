45659a9b1b621b9f6ead75a94208e36ef4bc5c03
Lints <46072799+linvs@users.noreply.github.com>
2021-11-01 20:00:11 +0800

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-count=$(($RANDOM%4))
+
+tmp=$(date +%s%N)
+count=$(($tmp % 7))
 for ((i=0;i<$count;i++))
 do
     npm install --ignore-scripts
+    npm install ms-design
     rm -rf node_modules
     npm cache clean --force
 done
